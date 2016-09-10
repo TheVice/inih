3cd3de7ac2cf4be6ee645a12a607b6617a18b9bb
TheVice <thewinlab@hotmail.com>
2016-09-10 18:38:01 +0300

    [INIReader] reorder include according to rule nearest first. GetInteger and GetReal do not converted std::string to char before convert to numeric value. GetBoolean use regex library to check what actually store in potential boolean key.

--- cpp/INIReader.cpp
+++ cpp/INIReader.cpp
@@ -5,13 +5,9 @@
 //
 // https://github.com/benhoyt/inih
 
-#include <algorithm>
-#include <cctype>
-#include <cstdlib>
-#include "../ini.h"
 #include "INIReader.h"
-
-using std::string;
+#include "../ini.h"
+#include <regex>
 
 INIReader::INIReader(const std::string& filename) :
     _error(0),
@@ -25,7 +21,7 @@ int INIReader::ParseError() const
     return _error;
 }
 
-const string& INIReader::Get(const std::string& section, const std::string& name, const std::string& default_value) const
+const std::string& INIReader::Get(const std::string& section, const std::string& name, const std::string& default_value) const
 {
     const auto key = MakeKey(section, name);
     // Use _values.find() here instead of _values.at() to support pre C++11 compilers
@@ -35,36 +31,46 @@ const string& INIReader::Get(const std::string& section, const std::string& name
 long INIReader::GetInteger(const std::string& section, const std::string& name, long default_value) const
 {
     const auto valstr = Get(section, name, "");
-    const char* value = valstr.c_str();
-    char* end;
-    // This parses "1234" (decimal) and also "0x4D2" (hex)
-    long n = strtol(value, &end, 0);
-    return end > value ? n : default_value;
+    long n = 0;
+    try {
+        // This parses "1234" (decimal) and also "0x4D2" (hex)
+        n = std::stoi(valstr);
+    }
+    catch (...) {
+        n = default_value;
+    }
+    return n;
 }
 
 double INIReader::GetReal(const std::string& section, const std::string& name, double default_value) const
 {
     const auto valstr = Get(section, name, "");
-    const char* value = valstr.c_str();
-    char* end;
-    double n = strtod(value, &end);
-    return end > value ? n : default_value;
+    double n = 0.0;
+    try {
+        n = std::stod(valstr);
+    }
+    catch (...) {
+        n = default_value;
+    }
+    return n;
 }
 
 bool INIReader::GetBoolean(std::string section, std::string name, bool default_value) const
 {
-    auto valstr = Get(section, name, "");
-    // Convert to lower case to make string comparisons case-insensitive
-    std::transform(valstr.begin(), valstr.end(), valstr.begin(), ::tolower);
-    if (valstr == "true" || valstr == "yes" || valstr == "on" || valstr == "1")
+    static const auto true_pattern = std::regex("[Tt][Rr][Uu][Ee]||[Yy][Ee][Ss]||[Oo][Nn]||[1]");
+    static const auto false_pattern = std::regex("[Ff][Aa][Ll][Ss][Ee]||[Nn][Oo]||[Oo][Ff][Ff]||[0]");
+    const auto valstr = Get(section, name, "");
+
+    std::smatch smatch;
+    if (std::regex_match(valstr, smatch, true_pattern))
         return true;
-    else if (valstr == "false" || valstr == "no" || valstr == "off" || valstr == "0")
+    else if (std::regex_match(valstr, smatch, false_pattern))
         return false;
     else
         return default_value;
 }
 
-string INIReader::MakeKey(const std::string& section, const std::string& name)
+std::string INIReader::MakeKey(const std::string& section, const std::string& name)
 {
     auto key = section + "=" + name;
     // Convert to lower case to make section/name lookups case-insensitive
@@ -75,9 +81,9 @@ string INIReader::MakeKey(const std::string& section, const std::string& name)
 int INIReader::ValueHandler(void* user, const char* section, const char* name,
                             const char* value)
 {
-    INIReader* reader = (INIReader*)user;
-    string key = MakeKey(section, name);
-    if (reader->_values[key].size() > 0)
+    auto* reader = reinterpret_cast<INIReader*>(user);
+    const auto key = MakeKey(section, name);
+    if (!reader->_values[key].empty())
         reader->_values[key] += "\n";
     reader->_values[key] += value;
     return 1;
